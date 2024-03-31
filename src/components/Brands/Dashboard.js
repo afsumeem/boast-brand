eeba24173b0b8b479d5d01b2f15318d5fef80db7
Afsana Meem <afsumeem@gmail.com>
2024-03-31 21:44:26 +0600

    dashboard updated

--- src/components/Brands/Dashboard.js
+++ src/components/Brands/Dashboard.js
@@ -9,9 +9,9 @@ const Dashboard = () => {
     });
   }, []);
   return (
-    <div className=" text-white mx-2 lg:mx-24 grid grid-cols-1 md:grid-cols-3 gap-4 mt-20 mb-10">
+    <div className=" text-white mx-2 lg:mx-24 grid grid-cols-1 md:grid-cols-3 gap-4 mt-20 mb-10 ">
       <div
-        data-aos="fade-right"
+        data-aos="fade-up"
         className=" text-white px-8 py-6 bg-[#0d1118] dashboard1"
         style={{ borderTopLeftRadius: "60px" }}
       >
@@ -42,7 +42,7 @@ const Dashboard = () => {
         <p>Than the industry average</p>
       </div>
       <div
-        data-aos="fade-left"
+        data-aos="fade-down"
         className="bg-[#0d1118] text-white px-8 py-6 rounded-br-[60px] dashboard3"
       >
         <h2
