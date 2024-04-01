f9594867e1134759b678a32c0b94f4225c74e7d0
Afsana Meem <afsumeem@gmail.com>
2024-04-01 17:07:08 +0600

    updated

--- src/components/Brands/HowItWorks.js
+++ src/components/Brands/HowItWorks.js
@@ -9,7 +9,7 @@ const HowItWorks = () => {
     slidesToShow: 1,
     slidesToScroll: 1,
     autoplay: true,
-    autoplaySpeed: 5000,
+    autoplaySpeed: 3000,
     nextArrow: <SampleNextArrow />,
     prevArrow: <SamplePrevArrow />,
   };
