33d04b9898cf79843a8bad47365252f1ad1e734f
Afsana Meem <afsumeem@gmail.com>
2024-04-02 16:19:54 +0600

    udpated

--- src/components/Brands/BrandHero.js
+++ src/components/Brands/BrandHero.js
@@ -17,7 +17,7 @@ const BrandHero = () => {
           />
         </div>
         <div className=" mx-2 lg:mx-24 h-screen ">
-          <div className=" grid grid-cols-1 lg:grid-cols-2 h-full mt-10 lg:-mt-20 gap-5">
+          <div className=" grid grid-cols-1 lg:grid-cols-2 h-full mt-10 lg:-mt-20 gap-5 px-10">
             <div
               className=" flex justify-center items-center img-container"
               onMouseEnter={() => setShowSecondImage(true)}
@@ -25,14 +25,14 @@ const BrandHero = () => {
             >
               <img
                 src="/hero/boastC.png"
-                className={`w-32 sm:w-40 md:w-52 lg:w-80 image ${
+                className={`w-32 -ml-32 sm:w-40 md:w-52 lg:w-80 image ${
                   showSecondImage ? "hidden" : ""
                 }`}
                 alt=""
               />
               <img
                 src="/hero/boastC2.png"
-                className={`w-32 sm:w-40 md:w-52 lg:w-80 image ${
+                className={`w-32 -ml-32 sm:w-40 md:w-52 lg:w-80 image ${
                   showSecondImage ? "" : "hidden"
                 }`}
                 alt=""
@@ -40,15 +40,15 @@ const BrandHero = () => {
             </div>
 
             <div className=" text-white flex justify-start items-center">
-              <div className="ml-0 lg:-ml-36 -mt-24">
+              <div className="ml-0 lg:-ml-32 -mt-12">
                 <h2 className="text-2xl sm:text-3xl md:text-4xl lg:text-6xl md:mt-0 font-extrabold text-center md:text-start ">
                   Supercharge your Brand
                 </h2>
-                <p className="font-light text-center md:text-start text:base lg:text-2xl my-5 text-gray-400">
+                <p className="font-bold text-center md:text-start text:base lg:text-2xl my-5 text-gray-400">
                   Using your customers and local influencers
                 </p>
 
-                <p className="font-thin  md:text-start text:base lg:text-xl 2xl:text-xl mb-5 md:mb-0 text-justify">
+                <p className="font-thin md:text-start text:base lg:text-xl 2xl:text-xl mb-5 md:mb-0 text-justify">
                   Boast provides automatching end-to-end UGC marketing for
                   businesses using their own customers and Influencers by
                   implementing a cashback model. We provide cashbacks from
